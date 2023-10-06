0a13a372d424bfa2acc9f8343623b74303e9290a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-06 12:26:17 +0600

    add style on virtuals

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
-import Animation from 'components/Animation/Animation';
+// import Animation from '.components/Animation/Animation';
+import Animation from './components/Animation/Animation';
 import './App.css';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
     <div >
+      {/* <Animation></Animation> */}
       <Animation></Animation>
-     
+      <Skills></Skills>
     </div>
   );
 }
