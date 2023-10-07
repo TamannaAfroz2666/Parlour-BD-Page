8a13951208eed7b4e6eb6b43bf2c990a94196ec8
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-07 09:23:17 +0600

    title subtitle add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,6 +8,19 @@ const Home = () => {
         <div className='homeHead'>
             <div className="homeSection">
                 <NavBar></NavBar>
+                <div className="home">
+                    <div className="content">
+                        <div className="title">
+                            <h1>Blossom Parlour</h1>
+                        </div>
+                        <div className="sub_title">
+                            <p>
+                                At the beauty parlour, we believe in a beauty of consious.We have created a salon that offers the highest quality hair services.
+                            </p>
+                        </div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
 
             </div>
 
