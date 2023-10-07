44aa03ab9464de69076b51b25f929ac7d9d3820a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-07 09:43:51 +0600

    add structure

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css';
 // import Navbar from './components/NavBar/Navbar';
 import NavBar from 'components/NavBar/NavBar';
+import playButton from '../../assets/image/PlayButton.jpg'
 
 const Home = () => {
     return (
@@ -18,10 +19,40 @@ const Home = () => {
                                 At the beauty parlour, we believe in a beauty of consious.We have created a salon that offers the highest quality hair services.
                             </p>
                         </div>
-                        <img src="" alt="" />
+                        <img src={playButton} alt="" />
                     </div>
                 </div>
 
+            </div>
+            <div className="info">
+                <div className="grid">
+                    <div className="col">
+                        <strong>Parlour</strong>
+                        <p>Blossoms Academy</p>
+                    </div>
+                    <div className="col">
+                        <strong>Email</strong>
+                        <p>Blossoms@gmail.com</p>
+                    </div>
+                    <div className="col">
+                        <strong>Mobile</strong>
+                        <p>013123465</p>
+                    </div>
+                    <div className="col">
+                        <strong>Address</strong>
+                        <p>Blossoms Academy, Dhanmondi 27, Dhaka</p>
+                    </div>
+                    <div className="col">
+                        <strong>Services</strong>
+                        <p>Sparking</p>
+                        <p>SPA cream</p>
+                    </div>
+                    <div className="col">
+                        <strong>Working Hours</strong>
+                        <p>Monday to Friday</p>
+                        <p>08:00 - 09:00</p>
+                    </div>
+                </div>
             </div>
 
         </div>
