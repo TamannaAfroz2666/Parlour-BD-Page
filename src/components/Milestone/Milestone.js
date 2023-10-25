398e4dc7edde69b6d96d0ae3ce84b894def386be
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-26 01:39:31 +0600

    working on new section

---
+++ src/components/Milestone/Milestone.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Milestone.css'
+
+const Milestone = () => {
+    return (
+        <div className='milestone'>
+            <div className="milestoneHead">
+                <div className="milestoneBody">
+                    <div className="milestoneContent">
+                        
+                    </div>
+                </div>
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default Milestone;
