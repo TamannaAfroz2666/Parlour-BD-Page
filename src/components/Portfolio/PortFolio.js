cc84cd61b697390da792cf2e9a42be3b73e30be8
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-25 11:00:49 +0600

    add motion

--- src/components/Portfolio/PortFolio.js
+++ src/components/Portfolio/PortFolio.js
@@ -7,18 +7,20 @@ const PortFolio = () => {
              <motion.div className='ShowTheDiv'
         initial={{
             opacity:0,
-            scale: 0
+            scale: 0,
+            x: 100
 
         }}
         whileInView={{
             opacity: 1,
-            scale: 1
+            scale: 1,
+            x: 0
 
         }}
         transition={{
             duration: 0.6
         }}
-        viewport={{once: true}}
+        // viewport={{once: true}}
         
         >
 
