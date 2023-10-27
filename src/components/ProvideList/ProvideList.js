352eebdedaa0388c082c400d313476cb9c050a62
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-27 11:57:42 +0600

    add base

--- src/components/ProvideList/ProvideList.js
+++ src/components/ProvideList/ProvideList.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import './ProvideList.css'
+import './ProvideList.css';
+import { BsFlower1 } from "react-icons/bs";
 
 const ProvideList = () => {
     return (
         <div className='provideList'>
-            
+            <div className="provideListHead">
+                <div className="provideBody">
+                    <p>What we provide</p>
+                    <div className="lineContents">
+                        <hr color='tomato' className='LineTitle'/>
+                        <div className="iconOfFlower">
+                            <BsFlower1 size={20} color='tomato' />
+
+                        </div>
+                        <hr color='tomato' className='LineTitle'/>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
