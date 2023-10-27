733e14bae2fba22465c7b27faea308276a98475c
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-27 12:19:44 +0600

    fixed the horizontal line issue

--- src/components/ProvideList/ProvideList.js
+++ src/components/ProvideList/ProvideList.js
@@ -9,12 +9,18 @@ const ProvideList = () => {
                 <div className="provideBody">
                     <p className='titleProvide'>What we provide</p>
                     <div className="lineContents">
+                        <div className="lineContentHead">
                         <hr color='tomato' className='LineTitle'/>
+                        </div>
+                     
                         <div className="iconOfFlower">
                             <BsFlower1 size={20} color='tomato' />
                         </div>
+                        <div className="lineContentHead">
                         <hr color='tomato' className='LineTitle'/>
+                        </div>
                     </div>
+
                 </div>
             </div>
 
