301ad1a80571346c2801351eef8cedf27f5a56d2
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-28 17:00:05 +0600

    add date picker

--- src/components/MakeBooking/MakeBooking.js
+++ src/components/MakeBooking/MakeBooking.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MakeBooking.css';
 import { BsFlower1 } from "react-icons/bs";
 import { HiOutlineChevronRight } from "react-icons/hi";
 import BookingInfo from './BookingInfo/BookingInfo';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const MakeBooking = () => {
+
+    // const [selectedDate, setSelectedDate] = useState('');
+    // const [selectedTime, setSelectedTime] = useState('');
+
+    // const handleDateChange = (e) => {
+    //   setSelectedDate(e.target.value);
+    // };
+
+    // const handleTimeChange = (e) => {
+    //   setSelectedTime(e.target.value);
+    // };
+
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
     return (
         <div className='makeBooking'>
             <div className="makeBookingHead">
@@ -36,13 +55,54 @@ const MakeBooking = () => {
                                     <input type="text" name='name' value='' className='emailField' placeholder='Phone No.' autoComplete='off' />
                                 </div>
                                 <div className="dateTime">
+                                    {/* 
+                                    <input type="date" value={selectedDate} onChange={handleDateChange} />
+                                    <select value={selectedTime} onChange={handleTimeChange}>
+                                        <option value="morning">Morning</option>
+                                        <option value="afternoon">Afternoon</option>
+                                        <option value="evening">Evening</option>
+                                    </select> */}
                                     <select name="dateTime" id="dateTime">
-                                        <option className='optionName' value="date">date time
-                                            <input type="date" />
+                                    <input type="text" />
+                                   
+                                        <option className='optionName' value="selectedDate"  >Select the date
+                                  
+                                        <h1>
 
+                                      
+                                         <DatePicker
+                                            selected={selectedDate}
+                                            onChange={handleDateChange}
+                                            dateFormat="MM/dd/yyyy" // Customize the date format as needed
+                                        />
+                                          </h1>
                                         </option>
+                                       
 
                                     </select>
+
+                                    {/* <div> */}
+                                        {/* <select>
+                                            <option value="option1">Option 1</option>
+                                            <option value="option2">Option 2</option>
+                                            <option value="option3">Option 3</option>
+                                        </select> */}
+                                        {/* <input
+                                            type="text"
+                                            placeholder="Select a date"
+                                            value={selectedDate}
+                                            onChange={(e) => e.preventDefault()}
+                                        /> */}
+                                        {/* <DatePicker
+                                            selected={selectedDate}
+                                            onChange={handleDateChange}
+                                            dateFormat="MM/dd/yyyy" // Customize the date format as needed
+                                        /> */}
+                                    {/* </div> */}
+
+
+
+
 
                                 </div>
                                 <div className="message">
---
+++ src/components/MakeBooking/MakeCalander/MakeCalander.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MakeCalander = () => {
+    return (
+        <div className='calander'>
+            <div className="caladrerHead">
+                
+            </div>
+            
+        </div>
+    );
+};
+
+export default MakeCalander;
