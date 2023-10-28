523e7b12dcee36c09c477f6c64d6be2db51a14d3
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-28 12:26:44 +0600

    booking components working

---
+++ src/components/MakeBooking/MakeBooking.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './MakeBooking.css'
+
+const MakeBooking = () => {
+    return (
+        <div className='makeBooking'>
+            <div className="makeBookingHead">
+                <div className="bookingBody">
+                    
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default MakeBooking;
