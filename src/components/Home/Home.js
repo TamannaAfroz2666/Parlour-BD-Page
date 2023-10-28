a789fa65da0b23794fa46d8be455d5eb4c920b89
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-28 12:42:50 +0600

    add base

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,7 @@ import Animation from 'components/Animation/Animation';
 import Milestone from 'components/Milestone/Milestone';
 import ProvideList from 'components/ProvideList/ProvideList';
 import Pricing from 'components/Pricing/Pricing';
+import MakeBooking from 'components/MakeBooking/MakeBooking';
 // import playButton from '../../assets/image/arrowPlayButton.PNG'
 // import {  PiNavigationArrowBold } from "react-icons/pi";
 
@@ -16,6 +17,7 @@ const Home = () => {
                 <Milestone></Milestone>
                 <ProvideList></ProvideList>
                 <Pricing></Pricing>
+                <MakeBooking></MakeBooking>
 
                 {/* <Animation></Animation> */}
 
--- src/components/MakeBooking/MakeBooking.js
+++ src/components/MakeBooking/MakeBooking.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import './MakeBooking.css'
+import './MakeBooking.css';
+import { BsFlower1 } from "react-icons/bs";
 
 const MakeBooking = () => {
     return (
         <div className='makeBooking'>
             <div className="makeBookingHead">
                 <div className="bookingBody">
-                    
+                    <div className="bookingContents">
+                        <div className="appoinment">
+                            <p className='appointmentTitle'>Make Appointment</p>
+                            <div className='IconSort'>
+                                <div className="iconOfFlower">
+                                    <BsFlower1 size={20} color='tomato' />
+
+                                </div>
+                                <div className="line">
+                                    <hr color='tomato' className='lineHr' />
+                                </div>
+
+                            </div>
+
+                        </div>
+                        <div className="appoinmentInfo">
+
+                        </div>
+                    </div>
+
                 </div>
             </div>
             
