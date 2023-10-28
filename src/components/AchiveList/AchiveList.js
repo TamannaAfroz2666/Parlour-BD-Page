dcbabda51d95537d6b10d53c0a18485d2a89b5a7
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-28 17:06:11 +0600

    base create

---
+++ src/components/AchiveList/AchiveList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './AchiveList.css';
+
+const AchiveList = () => {
+    return (
+        <div className='achiveList'>
+            <div className="achiveHead">
+                <div className="achiveBody">
+                    
+                </div>
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default AchiveList;
