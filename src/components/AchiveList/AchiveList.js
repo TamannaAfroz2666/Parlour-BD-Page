da301bb3b69457b6926919c35280e8be06ffb21a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-28 17:35:39 +0600

    review section has base create

--- src/components/AchiveList/AchiveList.js
+++ src/components/AchiveList/AchiveList.js
@@ -24,8 +24,22 @@ const AchiveList = () => {
                             </div>
                             {/* icon Flower start */}
 
-                            <p className='desAchive'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis nostrum dolores est sunt porro consequuntur necessitatibus rem? Molestiae, aliquid consectetur.</p>
-                            <p className='desAchive'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, aliquid.</p>
+                            <p className='desAchieve'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis nostrum dolores est sunt porro consequuntur necessitatibus rem? Molestiae, aliquid consectetur.</p>
+                            <p className='desAchieve'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias, aliquid.</p>
+
+
+                            {/* clients part start */}
+                            <div className="reviewContent">
+                                <div className="happyClients">
+                                    <p className='clientsNum' >4 <span className='upperNum'>8</span>71 <span className='upperPlus'>+</span></p>
+                                    <p className='clientsTitle'>Happy Clients</p>
+                                </div>
+                                <div className="relaxedClients">
+                                <p className='clientRexNum' >4 <span className='upperRexNum'>8</span>71 <span className='upperRexPlus'>+</span></p>
+                                    <p className='relaxedTitle'>Happy Clients</p>
+
+                                </div>
+                            </div>
 
 
                         </div>
