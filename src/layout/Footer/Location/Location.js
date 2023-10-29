6c1f3869183aaa613e4aa0630a508b868f09e5fd
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-30 00:43:22 +0600

    working on location

---
+++ src/layout/Footer/Location/Location.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Location.css'
+
+const Location = () => {
+    return (
+        <div className='location'>
+            <div className="locationContents">
+                
+            </div>
+            
+        </div>
+    );
+};
+
+export default Location;
