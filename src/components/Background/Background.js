2065597d048a21ebd7896149561f1491140fd613
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-29 22:50:46 +0600

    add mushroms field

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Background.css';
-import { GiGrapes , GiMushrooms, GiFlowerPot, GiMapleLeaf } from "react-icons/gi";
+import { GiGrapes, GiMushrooms, GiFlowerPot, GiMapleLeaf } from "react-icons/gi";
 
 const Background = () => {
     return (
@@ -9,28 +9,36 @@ const Background = () => {
                 <div className="backgroundContent">
                     <div className="contentsOfBg">
                         <div className="mushroomPart">
-                            <span> <GiMushrooms  color='white' size={70} /> </span>
-                            <p className='bgTitle'>natural</p>
-                            <p className='bgSubTitle'>Products</p>
+                            <span> <GiMushrooms color='white' size={70} /> </span>
+                            {/* <span className='bgHrHead'> <hr className='bgHr'/> </span> */}
+                            <div className="lineBreaker">
+                                <hr color='white' className='lineBreak1' />
+                            </div>
+                            <div className="bg_titlesContent">
+                                <p className='bgTitle'>natural</p>
+                                <p className='bgSubTitle'>Products</p>
+
+                            </div>
+
 
                         </div>
                         <div className="flowerPart">
-                            <span> <GiFlowerPot  color='white' size={70}/> </span>
+                            <span> <GiFlowerPot color='white' size={70} /> </span>
 
                         </div>
                         <div className="leafPart">
-                            <span> <GiMapleLeaf  color='white' size={70}  /> </span>
+                            <span> <GiMapleLeaf color='white' size={70} /> </span>
 
                         </div>
                         <div className="graphsPart">
-                            <span> <GiGrapes color='white' size={70}/> </span>
+                            <span> <GiGrapes color='white' size={70} /> </span>
 
                         </div>
                     </div>
-                    
+
                 </div>
-            </div> 
-            
+            </div>
+
         </div>
     );
 };
