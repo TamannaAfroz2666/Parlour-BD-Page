7a94ae5c8611e705f76df58b2c5500a768ac8d3e
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-29 21:14:18 +0600

    base create

---
+++ src/components/Background/Background.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Background.css';
+
+const Background = () => {
+    return (
+        <div className='backgroundHead'>
+            <div className="backgroundBody">
+                <div className="backgroundContent">
+                    
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Background;
