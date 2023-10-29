0bebd609ea772ac7171600e4299833010cdf2c08
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-29 23:21:14 +0600

    base create

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -3,6 +3,27 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='footer'>
+            <div className="footerHead">
+                <div className="footerMain">
+                    <div className="footerBody">
+                        <div className="footerContents">
+                            <div className="alisa">
+
+                            </div>
+                            <div className="ourLinks">
+
+                            </div>
+                            <div className="otherLinks">
+
+                            </div>
+                            <div className="otherContacts">
+                                
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
             
         </div>
     );
