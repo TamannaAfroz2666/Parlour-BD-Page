7df928e8648688f79be8b6d30bc2e827ae8e9bda
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-29 12:47:07 +0600

    link styled

--- src/components/Blog/PresentRating/PresentRating.js
+++ src/components/Blog/PresentRating/PresentRating.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './PresentRating.css';
 import image from '../../../assets/image/Pictures/Blogs/23.jpg';
 import { VscCalendar } from "react-icons/vsc";
-import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineUser, AiOutlineRight } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 const PresentRating = () => {
     return (
@@ -10,35 +11,48 @@ const PresentRating = () => {
             <div className="presentRtHead">
                 <div className="presentRtBody">
                     <div className="imageInfo">
-                    <img src={image} alt="" />
+                        <img src={image} alt="" />
                     </div>
                     <div className="timeAndDate">
                         <div className="presentDate">
                             <div className="dateRating">
-                            <VscCalendar  color='tomato' size={25}/>
-                            <p className='dateTitle'>7 March, 2020</p>
+                                <VscCalendar color='tomato' size={25} />
+                                <p className='dateTitle'>7 March, 2020</p>
 
                             </div>
                         </div>
                         <div className="userName">
                             <div className="userRating">
-                                <AiOutlineUser color='tomato' size={25}/>
+                                <AiOutlineUser color='tomato' size={25} />
                                 <p className='userTitle'>By Jony Doe</p>
                             </div>
                         </div>
                     </div>
                     <div className="lineBreaker">
-                        <hr  color='red' className='lineBreak'/>
+                        <hr color='red' className='lineBreak' />
                     </div>
                     <div className="ratingDesContent">
-                       <h1 className='desTitle'>Best Relaxation Body Massage Therapy</h1>
-                       <p className='desContent'>A full body massage is a form of massage therapy that involves massaging (almost!) the entire body, using professional techniques that can help to relieve muscle soreness, promote relaxation, and reduce stress.</p>
+                        <h1 className='desTitle'>Best Relaxation Body Massage Therapy</h1>
+                        <p className='desContent'>A full body massage is a form of massage therapy that involves massaging (almost!) the entire body, using professional techniques that can help to relieve muscle soreness, promote relaxation, and reduce stress.</p>
                     </div>
-                   
+                    <div className="readMoreRating">
+                        {/* <Link to='' >
+                            <AiOutlineRight  color='tomato' size={20}/>
+                            Read More
+
+                        </Link> */}
+                        <a href="/more">
+                        <AiOutlineRight  color='tomato' size={15} />
+                        <span className='moreLink'>  Read More</span>
+                          
+
+                        </a>
+                    </div>
+
 
                 </div>
             </div>
-            
+
         </div>
     );
 };
