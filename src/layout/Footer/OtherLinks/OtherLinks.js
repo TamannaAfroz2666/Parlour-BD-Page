2e3b00df36184e4dec95cd7bd4d250cab64699ae
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-30 00:37:05 +0600

    working on other link

---
+++ src/layout/Footer/OtherLinks/OtherLinks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './OtherLinks.css';
+
+const OtherLinks = () => {
+    return (
+        <div className='otherLinkHead'>
+            
+        </div>
+    );
+};
+
+export default OtherLinks;
