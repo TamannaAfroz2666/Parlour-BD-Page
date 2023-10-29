5128a3f03f4bed891cb025d4f51468aeeba39d21
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-29 10:28:16 +0600

    fixed the icon position

--- src/components/ClientSay/ClientSay.js
+++ src/components/ClientSay/ClientSay.js
@@ -243,24 +243,26 @@ const ClientSay = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                            <span class="visually-hidden">Previous</span>
-                                        </button>
-                                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                            <span class="visually-hidden">Next</span>
-                                        </button>
+                                        
                                     </div>
 
                                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="carousel-control-prev-icon" aria-hidden="true" id='id'></span>
+                                            <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true" id='id2'></span>
+                                            <span class="visually-hidden">Next</span>
+                                        </button>
+
+                                    {/* <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev" id='btnId'>
+                                    <span  class="carousel-control-prev-icon" aria-hidden="true" id='id'></span>
                                     <span class="visually-hidden">Previous</span>
                                 </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next" id='two'>
+                                    <span class="carousel-control-next-icon" aria-hidden="true" id='id2'></span>
                                     <span class="visually-hidden">Next</span>
-                                </button>
+                                </button> */}
 
 
 
