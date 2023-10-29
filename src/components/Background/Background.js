758391c487a35b7c0ec0c694f4cb7a5aa4b1cf82
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-29 22:39:41 +0600

    add icon and design title

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import './Background.css';
+import { GiGrapes , GiMushrooms, GiFlowerPot, GiMapleLeaf } from "react-icons/gi";
 
 const Background = () => {
     return (
         <div className='backgroundHead'>
             <div className="backgroundBody">
                 <div className="backgroundContent">
+                    <div className="contentsOfBg">
+                        <div className="mushroomPart">
+                            <span> <GiMushrooms  color='white' size={70} /> </span>
+                            <p className='bgTitle'>natural</p>
+                            <p className='bgSubTitle'>Products</p>
+
+                        </div>
+                        <div className="flowerPart">
+                            <span> <GiFlowerPot  color='white' size={70}/> </span>
+
+                        </div>
+                        <div className="leafPart">
+                            <span> <GiMapleLeaf  color='white' size={70}  /> </span>
+
+                        </div>
+                        <div className="graphsPart">
+                            <span> <GiGrapes color='white' size={70}/> </span>
+
+                        </div>
+                    </div>
                     
                 </div>
-            </div>
+            </div> 
             
         </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Home.css';
 // import Navbar from './components/NavBar/Navbar';
 import NavBar from 'components/NavBar/NavBar';
-import Animation from 'components/Animation/Animation';
+// import Animation from 'components/Animation/Animation';
 import Milestone from 'components/Milestone/Milestone';
 import ProvideList from 'components/ProvideList/ProvideList';
 import Pricing from 'components/Pricing/Pricing';
@@ -10,6 +10,7 @@ import MakeBooking from 'components/MakeBooking/MakeBooking';
 import AchiveList from 'components/AchiveList/AchiveList';
 import ClientSay from 'components/ClientSay/ClientSay';
 import Blog from 'components/Blog/Blog';
+import Background from 'components/Background/Background';
 // import playButton from '../../assets/image/arrowPlayButton.PNG'
 // import {  PiNavigationArrowBold } from "react-icons/pi";
 
@@ -24,6 +25,7 @@ const Home = () => {
                 <AchiveList></AchiveList>
                 <ClientSay></ClientSay>
                 <Blog></Blog>
+                <Background></Background>
 
                 {/* <Animation></Animation> */}
 
