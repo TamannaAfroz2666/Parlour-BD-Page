8748581427377a8457cc0d11522edbe7a235bb64
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-30 10:53:26 +0600

    email section have completed

--- src/layout/Footer/Location/Location.js
+++ src/layout/Footer/Location/Location.js
@@ -16,6 +16,26 @@ const Location = () => {
                     </div>
 
                 </div>
+                <div className="emailInfo">
+                    <div className="numberIcon">
+                       <span className='piEmail'> <PiPhoneCallFill size={20} color='white'/>  </span>
+                    </div>
+                    <div className="numberInfo">
+                        <p className='emailOf_Title'>info@gmail.com</p>
+                        <p className='email_of'>services@gmail.com</p>
+                    </div>
+
+                </div>
+                <div className="mobileInfo">
+                    <div className="numberIcon">
+                       <span className='pi'> <PiPhoneCallFill size={20} color='white'/>  </span>
+                    </div>
+                    <div className="numberInfo">
+                        <p className='numberOf_Title'>1800-880-3214</p>
+                        <p className='number_of'>+131 667 8645</p>
+                    </div>
+
+                </div>
 
             </div>
             
