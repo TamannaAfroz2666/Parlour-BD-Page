6da058c146cd720fb33205692e964cac0dddbafd
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-31 22:58:39 +0600

    working on new page

---
+++ src/Pages/Service/HomeService/HomeService.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomeService.css';
+
+const HomeService = () => {
+    return (
+        <div className='HomeService'>
+            
+        </div>
+    );
+};
+
+export default HomeService;
