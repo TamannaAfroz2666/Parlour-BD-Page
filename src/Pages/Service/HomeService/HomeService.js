570ace001261e9ae78794f87fbff25da7014fced
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-01 01:30:31 +0600

    add hover

--- src/Pages/Service/HomeService/HomeService.js
+++ src/Pages/Service/HomeService/HomeService.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HomeService.css';
+import { GiDrippingStar } from "react-icons/gi";
 
 const HomeService = () => {
     return (
@@ -7,7 +8,8 @@ const HomeService = () => {
             <div className="HomeServiseHead">
                 <div className="service_body">
                     <div className="service_content">
-                        <h1 className='hSTitle'>Alisa Salon</h1>
+                        <h1 className='hSTitle'> <span className='sar_name'>Alisa </span>Salon <span>
+                             <GiDrippingStar color='red'  size={30}/> </span></h1>
 
 
                     </div>
