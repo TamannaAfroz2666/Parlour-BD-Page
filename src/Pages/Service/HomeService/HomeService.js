ae3c6e5b2deb172bc03baf0540c9971754c4a1e8
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-01 01:36:37 +0600

    icon add

--- src/Pages/Service/HomeService/HomeService.js
+++ src/Pages/Service/HomeService/HomeService.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HomeService.css';
-import { GiDrippingStar } from "react-icons/gi";
+import { GiDrippingStar, GiButterflyFlower } from "react-icons/gi";
+// import {  GoWorkflow} from "react-icons/go";
 
 const HomeService = () => {
     return (
@@ -8,8 +9,13 @@ const HomeService = () => {
             <div className="HomeServiseHead">
                 <div className="service_body">
                     <div className="service_content">
-                        <h1 className='hSTitle'> <span className='sar_name'>Alisa </span>Salon <span>
+                        <h1 className='hSTitle'> <span className='sar_name'>Alisa </span> <br />Salon <span>
                              <GiDrippingStar color='red'  size={30}/> </span></h1>
+                             <div className="iconHomeS">
+                                <GiButterflyFlower color='tomato' size={20} />
+
+                             </div>
+                             <p>Our Service</p>
 
 
                     </div>
