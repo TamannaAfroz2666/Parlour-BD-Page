b82c793b32e06d95a67f1ccebb035e19e15dbc7e
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-10-31 12:41:14 +0600

    json file add

---
+++ src/Utilies/data.js
@@ -0,0 +1,31 @@
+export const result = {
+    "regularMember":{
+        title1: 'quality time your skin massaged',
+        title2 :'hair styled',
+        title3 :'best care for your skin and body',
+        title4 :'hair styled',
+        title5 :'You get custom information',
+        title6 :'salons have professional beauticians',
+        price: 49
+    },
+    "vipMember" :{
+        title1: 'quality time your skin massaged',
+        title2 :'hair styled',
+        title3 :'best care for your skin and body',
+        title4 :'hair styled',
+        title5 :'You get custom information',
+        title6 :'salons have professional beauticians',
+        price: 99
+    },
+    "premiumMember" : {
+        title1: 'quality time your skin massaged',
+        title2 :'hair styled',
+        title3 :'best care for your skin and body',
+        title4 :'hair styled',
+        title5 :'You get custom information',
+        title6 :'salons have professional beauticians',
+        price: 149
+
+    }
+
+}
--- src/components/Pricing/RegularMember/RegularMember.js
+++ src/components/Pricing/RegularMember/RegularMember.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './RegularMember.css';
 import regular from '../../../assets/image/add/Pricing/normal.jpg';
 import { HiOutlineChevronRight } from "react-icons/hi";
+import {result} from '../../../Utilies/data';
+import { useState } from 'react';
 
 const RegularMember = () => {
+    const [applyData, setApplyData] = useState(result);
+    console.log('data is ', result)
     return (
         <div className='regularMember'>
             <div className="regularHead">
@@ -11,7 +15,7 @@ const RegularMember = () => {
                 <p className='regularPrice'>Regular Member</p>
                 <hr className='regularHr' color='tomato' />
                 <ul className='listRegular'>
-                    <li className='titleRegular'>quality time your skin massaged </li>
+                    <li className='titleRegular'>{applyData.regularMember.title1}</li>
                     <li className='sortRegular'>hair styled </li>
                     <li className='titleRegular'> best care for your skin and body </li>
                     <li className='sortRegular2'>You get custom information </li>
