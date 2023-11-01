3d2b9d24776c32b65dcc9e9780d322944d9afead
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-01 21:32:15 +0600

    map using

--- src/Pages/PortfolioPage/PortfolioPageMain/ShowAll/ShowAll.js
+++ src/Pages/PortfolioPage/PortfolioPageMain/ShowAll/ShowAll.js
@@ -1,54 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ShowAll.css';
 import image1 from '../../../../assets/image/Pictures/Blogs/23.jpg'
-import image2 from '../../../../assets/image/Pictures/Blogs/HotSone.jpg'
+import image2 from '../../../../assets/image/Pictures/Blogs/HotSone.jpg';
+import data from '../../../../Utilies/showAll.json';
 
 const ShowAll = () => {
+    const [hairStyle, setHairStyle] = useState(data.hairStyle);
+    console.log('hair style', hairStyle);
+    const [color, setColor] = useState(data.coloring);
+    console.log('hair color  :', color);
+    const [products, setProducts] = useState(data.hairProducts);
+    console.log('hair products', products);
+
+    const allData = hairStyle.concat(color, products);
+    console.log('all data showing', allData);
+
+    const [showAllData, setShowAllData] = useState(allData);
+
+
     return (
         <div className='showAllPort'>
             <div className="headAllPort">
                 <div className="bodyAllPort">
-                    <div className="imgAll">
-                        <img src={image1} alt=""  className="image" />
-                        <div className="overlay">
-                            <div className="text">
-                                <h1>hello word</h1>
+
+                    {
+                        showAllData.slice(0, 9 ).map((dataInfo) =>{
+                            return(
+
+                                <div className="imgAll">
+                                <img src= {dataInfo.image} alt=""  className="image" />
+                                <div className="overlay">
+                                    <div className="text">
+                                        <h1 className='hell'>{dataInfo.name}</h1>
+                                        <h1 className='hell'>{dataInfo.description}</h1>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="imgAll">
-                        <img src={image2} alt="" />
-                    </div>
-                    <div className="imgAll">
-                        <img src={image1} alt="" />
-                    </div>
-                    <div className="imgAll">
-                        <img src={image2} alt="" />
-                    </div>
-                    <div className="imgAll">
-                        <img src={image1} alt="" />
-                    </div>
-                    <div className="imgAll">
-                        <img src={image2} alt="" />
-                    </div>
-                    <div className="imgAll">
-                        <img src={image1} alt="" />
-                    </div>
-                    <div className="imgAll">
-                        <img src={image2} alt="" />
-                    </div>
-                    <div className="imgAll">
-                        <img src={image1} alt="" />
-                    </div>
-                    <div className="imgAll">
-                        <img src={image2} alt="" />
-                    </div>
-                    <div className="imgAll">
-                        <img src={image1} alt="" />
-                    </div>
-                    <div className="imgAll">
-                        <img src={image2} alt="" />
-                    </div>
+                                
+                            )
+                        })
+                    }
+
+                   <div className="areaBtn">
+                    <button>
+                        Load more
+                    </button>
+                    </div> 
 
                 </div>
             </div>
