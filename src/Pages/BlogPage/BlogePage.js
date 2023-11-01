989a9464b428842b2184d6aa2ada44b5bf4c83dd
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-02 03:11:38 +0600

    blog page working on

---
+++ src/Pages/BlogPage/BlogePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BlogePage.css';
+
+const BlogePage = () => {
+    return (
+        <div className='blogPage'>
+            
+        </div>
+    );
+};
+
+export default BlogePage;
