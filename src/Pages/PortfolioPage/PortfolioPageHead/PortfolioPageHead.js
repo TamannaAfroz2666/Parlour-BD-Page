3adeed0a3e7fa5b6c588f2371d71037192a0c8da
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-01 16:17:43 +0600

    title designed

--- src/Pages/PortfolioPage/PortfolioPageHead/PortfolioPageHead.js
+++ src/Pages/PortfolioPage/PortfolioPageHead/PortfolioPageHead.js
@@ -6,6 +6,14 @@ const PortfolioPageHead = () => {
         <div className='portPHead'>
             <div className="portfolio_body">
                 <div className="portfolio_Content">
+                    <div className="title_content">
+                        <h2 className='title'>portfolio gallery</h2>
+
+                    </div>
+                    <div className="nav_content">
+
+                    </div>
+
 
                 </div>
             </div>
--- src/Pages/PortfolioPage/PortolioPage.js
+++ src/Pages/PortfolioPage/PortolioPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './PortfolioPage.css';
+import PortfolioPageHead from './PortfolioPageHead/PortfolioPageHead';
 
 const PortolioPage = () => {
     return (
         <div className='portfolioPage'>
             <div className="pHeadPage">
                 <div className="pBodyPage">
-                   
+                   <div className="head">
+                    <PortfolioPageHead></PortfolioPageHead>
+                   </div>
 
                 </div>
             </div>
