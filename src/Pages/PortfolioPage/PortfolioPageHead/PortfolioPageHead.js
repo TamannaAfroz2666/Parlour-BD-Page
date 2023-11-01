5fb8f71ec149e8cbf44d29ebd7bf77e929af8210
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-01 16:58:34 +0600

    animate add

--- src/Pages/PortfolioPage/PortfolioPageHead/PortfolioPageHead.js
+++ src/Pages/PortfolioPage/PortfolioPageHead/PortfolioPageHead.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import './PortfolioPageHead.css';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const PortfolioPageHead = () => {
     return (
         <div className='portPHead'>
             <div className="portfolio_body">
                 <div className="portfolio_Content">
-                    <div className="title_content">
+                    <motion.div
+                        className="title_content"
+                        initial={{ opacity: 0, x: '-100vh' }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{
+                            duration: 1,
+                            delay: 1
+                        }}
+
+                    >
                         <h2 className='title'>portfolio gallery</h2>
 
-                    </div>
+                    </motion.div>
                     <div className="nav_content">
                         <ul className='headLink'>
                             <li> <Link className='linkTitle' to='/'>Home </Link> </li>
-                            <span  className='linkTitle'>/</span>
+                            <span className='linkTitle'>/</span>
                             <li> <Link className='linkTitle' to='/portfolio'>Portfolio gallery</Link> </li>
                         </ul>
 
@@ -23,7 +33,7 @@ const PortfolioPageHead = () => {
 
                 </div>
             </div>
-            
+
         </div>
     );
 };
