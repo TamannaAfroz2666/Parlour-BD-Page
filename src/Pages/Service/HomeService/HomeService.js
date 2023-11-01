51dd8f74630ab46a6f57de8d7748b6784f1ff3f2
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-01 11:46:49 +0600

    add base

--- src/Pages/Service/HomeService/HomeService.js
+++ src/Pages/Service/HomeService/HomeService.js
@@ -19,8 +19,37 @@ const HomeService = () => {
                                 <p className='ourService'>Our Service</p>
                                 <span className="hrHome">
                                 <hr className='ourHr11' />
-
                                 </span>
+                                <div className="listOfSr">
+                                    <ul>
+                                        <li>VLCC facial</li>
+                                        <li>hair spa</li>
+                                        <li>pedicure</li>
+                                        <li>manicure</li>
+                                        <li>full hand wax</li>
+                                        <li>half leg wax</li>
+                                        <li>dtan face & hand</li>
+                                        <li>dtan full leg</li>
+                                    </ul>
+                                    <ul>
+                                        <li>dtan full leg</li>
+                                        <li>root touch-up</li>
+                                        <li>back massage</li>
+                                        <li>threading</li>
+                                        <li>uperlips</li>
+                                        <li>forehead cain</li>
+                                        <li>face wax</li>
+                                        <li>basic hair cut</li>
+                                        <li>bleach</li>
+                                    </ul>
+                                </div>
+                                <div className="aboutUs">
+                                    <h1 className='aboutUsTitleH'>About Us</h1>
+                                    <p>Relax yourself with our spa and massage service</p>
+                                </div>
+                               
+
+                            
                            
 
                             </div>
