85cefa4c288bda279e8e93a29feb4e54573e248d
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-03 01:58:39 +0600

    working on nails section

---
+++ src/Pages/Nails/Nails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nails.css'
+
+const Nails = () => {
+    return (
+        <div className='nails'>
+            
+        </div>
+    );
+};
+
+export default Nails;
