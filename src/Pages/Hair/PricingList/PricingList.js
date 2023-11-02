58d43af6a4c333d945ebcdef5989619db87de70b
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-02 21:00:39 +0600

    base create

--- src/Pages/Hair/PricingList/PricingList.js
+++ src/Pages/Hair/PricingList/PricingList.js
@@ -3,14 +3,23 @@ import './PricingList.css'
 
 const PricingList = () => {
     return (
-        <div className='pricingListPage1'>
-            <div className="listPrice1">
-                <h1 className='priceListTitle111'>Pricing list1</h1>
-                
-
+        <div className='pricingListPage'>
+            <div className="listPrice">
+                <h1 className='priceListTitle'>Pricing list</h1>
             </div>
             <div className="listInfoPrice">
-                <h3>tt</h3>
+                <div className="hairCutContents">
+                    <div className="nameCutting">
+                        <h3 className='cutTitle'>Women's hair cut</h3>
+                    </div>
+                    <div className="cutLine">
+                        <hr className='cutLineHr'/>
+                    </div>
+                    <div className="cuttingPrice">
+                        <p>tk 1200</p>
+                    </div>
+
+                </div>
 
             </div>
             
