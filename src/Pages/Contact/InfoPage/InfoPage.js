ee28c8a597ead1d33f289588d441e1631be5e407
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-02 16:43:59 +0600

    add working info page

---
+++ src/Pages/Contact/InfoPage/InfoPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './InfoPage.css';
+
+const InfoPage = () => {
+    return (
+        <div className='infoPage'>
+            
+            
+        </div>
+    );
+};
+
+export default InfoPage;
