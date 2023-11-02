8ede903b547fc2830c472cc0bf86ac1d75c09792
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-03 00:38:25 +0600

    base create

--- src/Pages/Hair/HairChart/HairChart.js
+++ src/Pages/Hair/HairChart/HairChart.js
@@ -5,9 +5,44 @@ const HairChart = () => {
     return (
         <div className='hairChart'>
             <div className="hairChartBody">
-                <h1>he</h1>
+                <div className="chartContent">
+                    <div className="color">
+                        <h1 className='colorTitle'>color</h1>
+                        <div className="colorContents">
+                            <div className="nameColor">
+                                <h3>single process</h3>
+                            </div>
+                            <div className="chartHr">
+                                <hr className='chartHr1'/>
+                            </div>
+                            <div className="colorPrice">
+                                <p className='colorPTitle'>tk 8000</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div className="highlight">
+                    <h1 className='colorTitle'>highlight</h1>
+                        <div className="colorContents">
+                            <div className="nameColor">
+                                <h3>single process</h3>
+                            </div>
+                            <div className="chartHr">
+                                <hr className='chartHr1'/>
+                            </div>
+                            <div className="colorPrice">
+                                <p className='colorPTitle'>tk 8000</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+               
             </div>
-            
+
         </div>
     );
 };
