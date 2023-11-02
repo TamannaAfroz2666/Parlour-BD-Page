d093e5b2c8de0454631a8a973129b277da8524c9
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-02 20:09:42 +0600

    image add

--- src/Pages/Hair/Hair.js
+++ src/Pages/Hair/Hair.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './Hair.css';
+import Header from 'layout/Header/Header';
+// import HeaderService from 'Pages/Service/HeaderService/HeaderService';
+// import HeadPage from 'Pages/Contact/HeadPage/HeadPage';
 
 const Hair = () => {
     return (
         <div className='hairContainer'>
+            <div className="hairHead">
+                <div className="hairBody">
+                    <Header></Header>
+                    <h1>hh</h1>
+                </div>
+
+            </div>
             
         </div>
     );
