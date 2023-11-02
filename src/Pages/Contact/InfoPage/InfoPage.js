286fd071370941d5abab5745cab8abfe3b6d9e60
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-02 19:06:32 +0600

    add location and email

--- src/Pages/Contact/InfoPage/InfoPage.js
+++ src/Pages/Contact/InfoPage/InfoPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './InfoPage.css';
+import { MdLocationPin, MdEmail } from "react-icons/md";
 
 const InfoPage = () => {
     return (
@@ -49,7 +50,35 @@ const InfoPage = () => {
                         <h1 className='titleInFo'>contact info</h1>
                         <p className='titleDes'>If you have any inquiries, comments, or would like to request an appointment, please call us or fill out this form and we will get back to you as soon as possible.</p>
 
+                        <div className="locationArea">
+                            <div className="location">
+                                <div className="icon">
+                                    <MdLocationPin color='black' size={25} />
+                                </div>
+                                <div className="info">
+                                    <p className='infoTitle'>451 Chestnut Ridge Rd, Woodcliff Lake, NJ 07677</p>
+
+                                </div>
+                            </div>
+                            <div className="emailInfo1">
+                            <div className="location">
+                                <div className="icon">
+                                    <MdEmail color='black' size={25}/>
+
+                                </div>
+                                <div className="info">
+                                    <p className='infoTitle'>alisa@mail.com</p>
+
+                                </div>
+                            </div>
+
+                            </div>
+                          
+                        </div>
+                        
+
                     </div>
+
 
                 </div>
 
