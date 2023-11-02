e173d6478934ad8610a9a61994b7e005e1e0d9d7
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-02 17:55:19 +0600

    add button and designed

--- src/Pages/Contact/InfoPage/InfoPage.js
+++ src/Pages/Contact/InfoPage/InfoPage.js
@@ -6,30 +6,43 @@ const InfoPage = () => {
         <div className='infoPage'>
             <div className="infoP_head">
                 <div className="infoBodyContact">
+
                     <div className="contactInput">
-                        <div className="nameField">
-                            <input
-                                type="text"
-                                name='name'
-                                placeholder='Name'
-                                className='inputField'
-                            />
-                        </div>
-                        <div className="emailField">
-                            <input
-                                type="email"
-                                name='email'
-                                placeholder='Email'
-                                className='inputField'
-                            />
-                        </div>
-                        <div className="massageField">
-                            <textarea
-                                type="text"
-                                name='message'
-                                placeholder='Write a message' className='textAreaField'
-                            />
-                        </div>
+                        <form action="">
+
+                            <div className="nameField">
+                                <input
+                                    type="text"
+                                    name='name'
+                                    placeholder='Name'
+                                    className='inputField'
+                                />
+                            </div>
+                            <div className="emailField">
+                                <input
+                                    type="email"
+                                    name='email'
+                                    placeholder='Email'
+                                    className='inputField'
+                                />
+                            </div>
+                            <div className="massageField">
+                                <textarea
+                                    type="text"
+                                    name='message'
+                                    placeholder='Write a message' className='textAreaField'
+                                />
+                            </div>
+
+                            <div className="priceRegBtn">
+                                <button className='btnSubInfoReg'
+
+                                    type='submit'>Send</button>
+                            </div>
+
+
+
+                        </form>
 
                     </div>
                     <div className="infoContents">
