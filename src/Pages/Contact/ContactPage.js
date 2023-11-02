cfbb49853a8ba358f0d954776398e6262e0b6521
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-11-02 15:19:05 +0600

    working on contact page

---
+++ src/Pages/Contact/ContactPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ContactPage.css';
+
+const ContactPage = () => {
+    return (
+        <div className='contactPHead'>
+            <div className="contactBodyPage">
+                <div className="contactPage">
+                    
+
+
+                </div>
+                
+            </div>
+            
+        </div>
+    );
+};
+
+export default ContactPage;
